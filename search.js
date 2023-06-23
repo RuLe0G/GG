4822db90850b35615153e396e2d368a4e05ad437
RuLe0G <khlebnikov2015@yandex.ru>
2023-06-23 18:39:00 +0700

    Revert "upd search"
    This reverts commit bfb0bdd459c04fdaf145c924417b0e5a6970760c.

--- search.js
+++ search.js
@@ -1,5 +1,5 @@
 const xhr = new XMLHttpRequest();
-xhr.open('GET', 'steam_data.json', true);
+xhr.open('GET', '/steam_data.json', true);
 xhr.onload = function() {
   if (xhr.status === 200) {
     const data = JSON.parse(xhr.responseText);
@@ -8,17 +8,23 @@ xhr.onload = function() {
     const searchInput = document.getElementById('search-input');
     const suggestionsList = document.getElementById('suggestions-list');
 
+    let selectedAppId; // Глобальная переменная для сохранения appid
+
     searchInput.addEventListener('input', () => {
       const userInput = searchInput.value.toLowerCase();
       const filteredSuggestions = suggestions.filter(suggestion => suggestion.name.toLowerCase().startsWith(userInput));
 
       suggestionsList.innerHTML = '';
 
-      filteredSuggestions.forEach(suggestion => {
+      // Ограничение на вывод первых 5 подсказок
+      const limitedSuggestions = filteredSuggestions.slice(0, 5);
+
+      limitedSuggestions.forEach(suggestion => {
         const li = document.createElement('li');
         li.textContent = suggestion.name;
         li.addEventListener('click', () => {
           searchInput.value = suggestion.name;
+          selectedAppId = suggestion.appid; // Сохранение appid в глобальную переменную
           suggestionsList.innerHTML = '';
         });
         suggestionsList.appendChild(li);
@@ -27,3 +33,9 @@ xhr.onload = function() {
   }
 };
 xhr.send();
+
+const searchButton = document.getElementById('search-button');
+searchButton.addEventListener('click', () => {
+  console.log("in ${selectedAppId}");
+  console.log(selectedAppId);
+});
