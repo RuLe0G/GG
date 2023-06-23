8cf6c023f0a54b1cc1dfdd0a8d03ad49ea77cc02
RuLe0G <khlebnikov2015@yandex.ru>
2023-06-23 21:18:44 +0700

    add steamspy

--- search.js
+++ search.js
@@ -1,13 +1,14 @@
 const xhr = new XMLHttpRequest();
 let selectedAppId = 4;
-xhr.open('GET', '/steam_data.json', true);
+xhr.open('GET', '/clean_steamspy_data.json', true);
 xhr.onload = function() {
   if (xhr.status === 200) {
     const data = JSON.parse(xhr.responseText);
     const suggestions = data.applist.apps;
-    
+
     const searchInput = document.getElementById('search-input');
     const suggestionsList = document.getElementById('suggestions-list');
+    const infoBlock = document.querySelector('.info-blocks .info-block'); // Выбираем первый info-block
 
     searchInput.addEventListener('input', () => {
       const userInput = searchInput.value.toLowerCase();
@@ -28,23 +29,18 @@ xhr.onload = function() {
         suggestionsList.appendChild(li);
       });
     });
+
+    const searchButton = document.getElementById('search-button');
+    searchButton.addEventListener('click', () => {
+      const selectedApp = suggestions.find(suggestion => suggestion.appid === selectedAppId);
+      if (selectedApp) {
+        // Получаем информацию об элементе с выбранным appid
+        const appDetails = data[selectedAppId];
+
+        // Выводим информацию в первый info-block
+        infoBlock.textContent = JSON.stringify(appDetails);
+      }
+    });
   }
 };
-const searchButton = document.getElementById('search-button');
-searchButton.addEventListener('click', () => {
-  const infoBlock = document.querySelector('.info-blocks .info-block');
-  const appDetailsUrl = `https://steamspy.com/api.php?request=appdetails&appid=${selectedAppId}`;
-
-  // Выполнение AJAX-запроса к URL appDetailsUrl
-  const appDetailsXhr = new XMLHttpRequest();
-  appDetailsXhr.open('GET', appDetailsUrl, true);
-  appDetailsXhr.onload = function() {
-    if (appDetailsXhr.status === 200) {
-      const appDetails = JSON.parse(appDetailsXhr.responseText);
-      infoBlock.textContent = JSON.stringify(appDetails);
-    }
-  };
-  appDetailsXhr.send();
-});
 xhr.send();
-
